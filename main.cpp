10953e576af11ac7a02352b40dcee645f6f94041
Dmitry Kutsenko <d2emonium@gmail.com>
2016-11-26 23:28:04 +0400

    Generate figures

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <SFML/Graphics.hpp>
 
+const int M=20;
+const int N=10;
+
+int field[M][N]={0};
+
+struct Point
+{int x, y;} a[4], b[4];
+
+int figures[7][4]={
+    1, 3, 5, 7, // I
+    2, 4, 5, 7, // Z
+    3, 4, 5, 6, // S
+    3, 4, 5, 7, // T
+    2, 4, 6, 7, // L
+    3, 5, 6, 7, // J
+    2, 3, 4, 5, // O
+};
+
 int main()
 {
     // Create the main window
@@ -26,11 +44,23 @@ int main()
                 app.close();
         }
 
+        int n=3;
+        for(int i=0; i<4; i++)
+        {
+            a[i].x = figures[n][i] % 2;
+            a[i].y = figures[n][i] / 2;
+        }
+
         // Clear screen
         app.clear(sf::Color::White);
 
-        // Draw the sprite
-        app.draw(sprite);
+        for(int i=0; i<4; i++)
+        {
+            sprite.setPosition(a[i].x * 18, a[i].y * 18);
+
+            // Draw the sprite
+            app.draw(sprite);
+        }
 
         // Update the window
         app.display();
